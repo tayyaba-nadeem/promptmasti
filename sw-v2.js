e8354bb694132e95a75722b2e8c6e05a1580e008
Tayyaba Nadeem <tayyaba.nadeem.hsp@gmail.com>
2025-11-08 22:44:57 +0500

    Update and rename sw_v1.js to sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker for Prompt Masti
-const CACHE_NAME = 'promptmasti-v2.0';
+const CACHE_NAME = 'promptmasti-v2.1';
 const urlsToCache = [
   '/',
   '/?m=1',
@@ -9,14 +9,10 @@ const urlsToCache = [
   'https://cdn.statically.io/gh/tayyaba-nadeem/promptmasti/main/favicon-512x512.png'
 ];
 
-// Install event
 self.addEventListener('install', function(event) {
-  console.log('Service Worker: Installing...');
-  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Service Worker: Caching essential files');
         return cache.addAll(urlsToCache);
       })
       .then(function() {
@@ -25,16 +21,12 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// Activate event
 self.addEventListener('activate', function(event) {
-  console.log('Service Worker: Activating...');
-  
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheName !== CACHE_NAME) {
-            console.log('Service Worker: Deleting old cache', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -45,17 +37,13 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-// Fetch event
 self.addEventListener('fetch', function(event) {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
   );
 });
